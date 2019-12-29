48755497717fb74319b17e79f152cd4f534eba76
wengxk <wengxxk@gmail.com>
2019-12-29 23:33:07 +0800

    init commit

---
+++ AccountServer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AccountServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
